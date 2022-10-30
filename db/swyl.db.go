f774f04730bfdc1bc1594f8661719a67447b23af
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-30 15:28:03 -0400

    Added EstablishMongoClient api to package db

---
+++ db/swyl.db.go
@@ -0,0 +1,41 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package db
+
+// @import
+import (
+	"Swyl/server/utils"
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// @dev Creates a MongoDB instance
+//
+// @return *mongo.Client
+func EstablishMongoClient(ctx context.Context) *mongo.Client {
+	// get the mongoDB uri
+	mongoUri := os.Getenv("MONGODB_URI")
+	if mongoUri == "" {log.Fatal("!MONGODB_URI - uri is not defined.")}
+	
+	// Establish the connection
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+	utils.HandleException(err)
+
+	// defer a call to `Disconnect()` after instantiating client
+	defer func() {if err := mongoClient.Disconnect(ctx); err != nil {panic(err)}}()
+
+	// return mongo client
+	log.Println("MongoDB connected...")
+	return mongoClient
+}
+
