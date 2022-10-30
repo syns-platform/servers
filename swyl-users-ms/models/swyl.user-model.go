09b5933b5814b5a023f4b2653f8ae726c0b6e87c
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-30 17:04:42 -0400

    Create swyl.user-model.go

---
+++ swyl-users-ms/models/swyl.user-model.go
@@ -0,0 +1,30 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// @notice The information related to a Swyl user
+type User struct {
+	ID				primitive.ObjectID			`bson:"_id"`
+	Wallet_address 			string					`json:"wallet_address" bson:"wallet_address" validate:"required, min:42, max:42"`
+	Username 			string					`json:"username" bson:"username" validate:"required, min:3, max:20"`
+	Display_name 			string					`json:"display_name" bson:"display_name" validate:"min:3, max:40"`
+	Email 				string					`json:"email" bson:"email" validate:"min:3, max:40"`
+	Bio 				string					`json:"bio" bson:"bio" validate:"max: 200"`
+	Website 			string					`json:"website" bson:"website"`
+	Social_media 			[]SocialMedia				`json:"social_media" bson:"social_media"`
+}
+
+
+// @notice The information related to the links to a user's social media
+type SocialMedia struct {
+	Media 				string 					`json:"media" bson:"media"`
+	Url				string					`json:"url" bson:"url"`
+}
