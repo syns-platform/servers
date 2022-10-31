ea71444f78e49bc1aca6830df7bae58e589f4a47
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-31 19:09:47 -0400

    #club/club - implemented CreateClub api in controller

--- swyl-club-ms/controllers/club/swyl.club-controller.go
+++ swyl-club-ms/controllers/club/swyl.club-controller.go
@@ -11,9 +11,12 @@ package controllers
 // @import
 import (
 	dao "Swyl/servers/swyl-club-ms/dao/club"
+	"Swyl/servers/swyl-club-ms/models"
+	"Swyl/servers/swyl-club-ms/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 )
 
 // @notice Root struct for other methods in club-constrollers
@@ -38,7 +41,30 @@ func ClubControllerConstructor(clubDao dao.ClubDao) *ClubController {
 // 
 // @param gc *gin.Context
 func (cc *ClubController) CreateClub(gc *gin.Context) {
-	gc.JSON(http.StatusOK, gin.H{"mes": "swylv1.0"})
+	// declare params 
+	var params *models.Club
+
+	// bind json post data to params
+	if err := gc.ShouldBindJSON(&params); err != nil {
+		gc.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return;
+	}
+
+	// extra validation on struct models.Club
+	validate := validator.New()
+	if err := validate.Struct(params); err != nil {gc.AbortWithStatusJSON(http.StatusBadRequest, err.Error()); return;}
+
+	// test params.Club_owner to match ETH Crypto wallet address convention
+	matched, err := utils.TestEthAddress(params.Club_owner)
+	if err != nil{gc.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "!REGEX - cannot test wallet_address against regex"}); return;}
+	if !matched {gc.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "!ETH_ADDRESS - wallet_address is not an ETH crypto wallet address"}); return;}
+
+	// invoke ClubDaoImpl.CreateClub() api
+	if err := cc.ClubDao.CreateClub(params.Club_owner); err != nil {
+		gc.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()}); return;
+	}
+
+	// http response
+	gc.JSON(http.StatusOK, gin.H{"msg": "Club sucessfully created"})
 }
 
 
