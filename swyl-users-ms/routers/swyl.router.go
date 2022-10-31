891fdc30eebdf7475bb323fac0051c5247ff4781
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-30 20:36:26 -0400

    Create swyl.router.go

---
+++ swyl-users-ms/routers/swyl.router.go
@@ -0,0 +1,40 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package routers
+
+// @import
+import (
+	"Swyl/servers/swyl-users-ms/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @notice Root struct for other methods in router
+type UserRouter struct {
+   UserController *controllers.UserController
+}
+
+// @dev Constructor
+func UserRouterConstructor(userController controllers.UserController) *UserRouter {
+   return &UserRouter{
+      UserController: &userController,
+   }
+}
+
+
+// @notice Method of UserController struct
+// 
+// @dev Declares a list of endpoints
+func (ur * UserRouter) UserRoutes(rg *gin.RouterGroup) {
+   rg.POST("/connect", ur.UserController.Connect)
+   rg.GET("/get-user-at/:wallet-address", ur.UserController.GetUserAt)
+   rg.GET("/get-all-user", ur.UserController.GetAllUsers)
+   rg.PATCH("/update-user", ur.UserController.UpdateUser)
+   rg.DELETE("/deactivate-user-at/:wallet-address", ur.UserController.DeactivateUserAt)
+}
