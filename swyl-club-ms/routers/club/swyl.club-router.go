e2d844913491ec71d00342c52b52ffe67f7ecc96
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-31 17:10:34 -0400

    #club/club - init club-router

---
+++ swyl-club-ms/routers/club/swyl.club-router.go
@@ -0,0 +1,38 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package routers
+
+// @import
+import (
+	controllers "Swyl/servers/swyl-club-ms/controllers/club"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @notice Root struct for other methods in router
+type ClubRouter struct {
+	ClubController *controllers.ClubController
+}
+
+
+// @dev Constructor
+func ClubRouterConstructor(clubController *controllers.ClubController) *ClubRouter {
+	return &ClubRouter{
+		ClubController: clubController,
+	}
+}
+
+// @notice Method of ClubRouter struct
+// 
+// @dev Decalres a list of endpoints
+func (cr *ClubRouter) ClubRoutes(rg *gin.RouterGroup) {
+	rg.POST("/create-club", cr.ClubController.CreateClub)
+	rg.GET("/get-club-owned-by/:club-owner", cr.ClubController.GetClubOwnedBy)
+	rg.PATCH("/update-club", cr.ClubController.UpdateClub)
+}
