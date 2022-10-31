27ca38a2941d41b85df9758c7a9267321eb79110
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-31 14:57:44 -0400

    Basic gin set up

--- swyl-club-ms/swyl.main.go
+++ swyl-club-ms/swyl.main.go
@@ -8,18 +8,42 @@
 // @package
 package main
 
+// @import
 import (
-	"net/http"
+	"Swyl/servers/swyl-club-ms/utils"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+// @notice global variables
+var (
+	server			*gin.Engine
+)
+
+
+// @dev Runs before main()
+func init() {
+   // load env variables
+	if (os.Getenv("GIN_MODE") != "release") {utils.LoadEnvVars()}
+   
+   // set up gin engine
+	server = gin.Default()
+
+	// Gin trust all proxies by default and it's not safe. Set trusted proxy to home router to to mitigate 
+	server.SetTrustedProxies([]string{os.Getenv("HOME_ROUTER")})
+}
+
+
+// @dev Root function
 func main() {
-   r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() 
+   // Catch all unallowed HTTP methods sent to the server
+	server.HandleMethodNotAllowed = true
+
+	// run server
+	if (os.Getenv("GIN_MODE") != "release") {
+		server.Run(os.Getenv("SOURCE_IP"))
+	} else {
+		server.Run(":"+os.Getenv("PRODUCTION_PORT"))
+	}
 }
