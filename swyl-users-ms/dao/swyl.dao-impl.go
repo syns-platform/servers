e16c5890e4ca7a733f2b42fb5761a9c971e5a87d
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-31 11:21:03 -0400

    Updated Connect() api in dao-impl

--- swyl-users-ms/dao/swyl.dao-impl.go
+++ swyl-users-ms/dao/swyl.dao-impl.go
@@ -12,7 +12,9 @@ package dao
 import (
 	"Swyl/servers/swyl-users-ms/models"
 	"context"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -39,9 +41,41 @@ func UserDaoConstructor(ctx context.Context, mongoCollection *mongo.Collection) 
 // 
 // @param user	*models.User
 // 
+// @return *models.User
+// 
 // @return error
-func (ui *UserDaoImpl) Connect(user *models.User) error {
-	return nil
+// 
+// @TODO should return JWT token
+func (ui *UserDaoImpl) Connect(walletAddress *string) (*models.User, error) {
+	// declare user placeholder
+	var user *models.User = &models.User{}
+
+	// set up find query
+	query := bson.D{{Key: "wallet_address", Value: walletAddress}}
+	
+	// find the user in database using user.wallet_address
+	dbResult := ui.mongoCollection.FindOne(ui.ctx, query).Decode(user)
+
+	// logic: if dbResult error == nil => user with `walletAddress` has already connected before
+	// logic: if dbResult error != nil => user with `walletAddress` has never connected before
+	if (dbResult == nil) {
+		return user, nil
+	} else if dbResult.Error() == "mongo: no documents in result" {
+		// prepare user
+		user = &models.User{
+			Wallet_address: walletAddress,
+			Username: walletAddress,
+			Joined_at: time.Now().Unix(),
+		}
+
+		// insert new user to internal database
+		_, err := ui.mongoCollection.InsertOne(ui.ctx, user)
+
+		// return
+		return user, err
+	} else {
+		return nil, dbResult
+	}
 }
 
 
