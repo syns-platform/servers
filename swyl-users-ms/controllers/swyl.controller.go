3452c12e6ed8d75e0d202fa79b318210ad5339d3
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-30 20:35:56 -0400

    Update swyl.controller.go

--- swyl-users-ms/controllers/swyl.controller.go
+++ swyl-users-ms/controllers/swyl.controller.go
@@ -11,10 +11,11 @@ package controllers
 // @import
 import (
 	"Swyl/servers/swyl-users-ms/dao"
-	"Swyl/servers/swyl-users-ms/models"
+
+	"github.com/gin-gonic/gin"
 )
 
-// @notice Root struct for other methods in controllers
+// @notice Root struct for other methods in controller
 type UserController struct {
 	UserDao dao.UserDao
 }
@@ -29,51 +30,50 @@ func UserControllerConstructor(userDao dao.UserDao) *UserController{
 
 // @notice Method of UserController struct
 // 
-// @dev
+// @route `POST/connect`
+// 
+// @dev Connects to an account stored in the internal database using wallet address. Create a new account on first connect.
 // 
 // @param gc *gin.Context
-// 
-// @return error
-func (uc *UserController) Connect(user *models.User ){}
+func (uc *UserController) Connect(gc *gin.Context){}
 
 
 // @notice Method of UserController struct
 // 
-// @dev
+// @route `GET/get-user-at`
+// 
+// @dev Respond with a user at param `wallet-address`
 // 
 // @param gc *gin.Context
-// 
-// @return *models.User
-// 
-// @return 
-func (uc *UserController) GetUserAt(walletAddress *string) {}
+func (uc *UserController) GetUserAt(gc *gin.Context) {}
 
 
 // @notice Method of UserController struct
 // 
-// @dev
-// 
+// @route `GET/get-all-users`
 // @NOTE might not be necessary
 // 
-// @return []*models.User
-func (uc *UserController) GetAllUsers() {}
-
-
-// @notice Method of UserController struct
-// 
-// @dev
+// @dev Reponsd with a slice of models.User
 // 
 // @param gc *gin.Context
-// 
-// @return `r
-func (uc *UserController) UpdateUser(*models.User) {}
+func (uc *UserController) GetAllUsers(gc *gin.Context) {}
 
 
 // @notice Method of UserController struct
 // 
-// @dev
+// @route `PATCH/update-user`
+// 
+// @dev Allows a user to update their account
+// 
+// @param gc *gin.Context
+func (uc *UserController) UpdateUser(gc *gin.Context) {}
+
+
+// @notice Method of UserController struct
+// 
+// @route `DELETE/deactivate-user-at/:wallet-address`
 // 
 // @param gc *gin.Context
 // 
 // @return error
-func (uc *UserController) DeleteUserAt(walletAddress *string) {}
+func (uc *UserController) DeactivateUserAt(gc *gin.Context) {}
