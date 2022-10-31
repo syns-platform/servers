df9a2455c4eed57f508e75e17f1712e553d14c4c
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-31 11:15:09 -0400

    Updated type User struct

--- swyl-users-ms/models/swyl.user-model.go
+++ swyl-users-ms/models/swyl.user-model.go
@@ -8,23 +8,16 @@
 // @package
 package models
 
-import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 // @notice The information related to a Swyl user
 type User struct {
-	ID				primitive.ObjectID			`bson:"_id"`
-	Wallet_address 			string					`json:"wallet_address" bson:"wallet_address" validate:"required, min:42, max:42"`
-	Username 			string					`json:"username" bson:"username" validate:"required, min:3, max:20"`
-	Avatar				string					`json:"avatar" bson:"avatar"`	
-	Display_name 			string					`json:"display_name" bson:"display_name" validate:"min:3, max:40"`
-	Email 				string					`json:"email" bson:"email" validate:"min:3, max:40"`
-	Bio 				string					`json:"bio" bson:"bio" validate:"max: 200"`
-	Website 			string					`json:"website" bson:"website"`
-	Joined_at			time.Time				`json:"joined_at" bson:"joined_at"`
+	Wallet_address 			*string					`json:"wallet_address" bson:"wallet_address" validate:"required,len=42,alphanum"`
+	Username 			*string					`json:"username" bson:"username" validate:"omitempty,min=3,max=20"`
+	Avatar				*string					`json:"avatar" bson:"avatar"`	
+	Display_name 			*string					`json:"display_name" bson:"display_name" validate:"omitempty,min=3,max=40"`
+	Email 				*string					`json:"email" bson:"email" validate:"omitempty,min=3,max=40"`
+	Bio 				*string					`json:"bio" bson:"bio" validate:"omitempty,max=200"`
+	Website 			*string					`json:"website" bson:"website"`
+	Joined_at			int64				`json:"joined_at" bson:"joined_at"`
 	Social_media 			[]SocialMedia				`json:"social_media" bson:"social_media"`
 }
 
