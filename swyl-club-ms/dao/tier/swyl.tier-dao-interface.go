8017974978f4cc8d2089de2f46f77007f1c4de37
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-01 00:35:15 -0400

    #club/tier - init dao-interface

--- swyl-club-ms/dao/tier/swyl.tier-dao-interface.go
+++ swyl-club-ms/dao/tier/swyl.tier-dao-interface.go
@@ -8,3 +8,54 @@
 // @package
 package dao
 
+// @import
+import "Swyl/servers/swyl-club-ms/models"
+
+// @notice Dao interface
+type TierDao interface {
+
+   // @notice Lets a club owner create a tier to internal database
+	// 
+	// @param tier *models.Tier
+	// 
+	// @return error
+   CreateTier(tier *models.Tier) error
+
+   // @notice Gets a Tier at tierId and clubOwner
+	// 
+	// @param clubId *uint64
+	// 
+	// @param clubOwner *string
+	// 
+	// @return *models.Tier
+	// 
+	// @return error
+   GetTierAt(clubId *uint64, clubOwner *string) (*models.Tier, error)
+
+
+   // @notice Gets all tiers owned by clubOwner
+	// 
+	// @param clubOwner *string
+	// 
+	// @return *[]models.Tier
+	// 
+	// @return error
+   GetAllTiersOwnedBy(clubOwner *string) (*[]models.Tier, error)
+
+
+   // @notice Lets a clubOwner update a tier
+	// 
+	// @param tier *models.Tier
+	// 
+	// @return error
+   UpdateTier(tier *models.Tier) error
+
+   // @notice Lets a clubOwner delete a tier
+	// 
+	// @param tierId *uint64
+	// 
+	// @param clubOwner *string
+	// 
+	// @return error
+   DeleteTier(clubId *uint64, clubOwner *string) error
+}
