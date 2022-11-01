5b7208c90f1f8e02a2aa0cd1b077dea1994c91ee
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-01 00:34:22 -0400

    #club/tier - updated models

--- swyl-club-ms/models/swyl.models .go
+++ swyl-club-ms/models/swyl.models .go
@@ -18,14 +18,14 @@ type Club struct {
 
 // @notice The information related to a Swyl Tier
 type Tier struct {
-	Club_ID						*uint64 	`json:"club_id" bson:"club_id" validate:"required"`
+	Club_owner					*string 	`json:"club_owner" bson:"club_owner" validate:"required,len=42,alphanum"`
 	Tier_ID						*uint64		`json:"tier_id" bson:"tier_id" validate:"required"`
 	Tier_name					*string		`json:"tier_name" bson:"tier_name" validate:"required,min=2,max=20"`
 	Tier_img					*string		`json:"tier_img" bson:"tier_img"`
 	Tier_bio					*string		`json:"tier_bio" bson:"tier_bio" validate:"omitempty,min=2,max=200"`
-	Tier_fee					*uint64		`json:"tier_fee" bson:"tier_fee"`
+	Tier_fee					*uint64		`json:"tier_fee" bson:"tier_fee" validate:"required"`
 	Tier_welcome_msg				*string		`json:"Tier_welcome_msg" bson:"Tier_welcome_msg" validate:"omitempty,min=2,max=100"`
-	Created_at					*uint64		`json:"created_at" bson:"created_at"`
+	Created_at					uint64		`json:"created_at" bson:"created_at"`
 }
 
 
