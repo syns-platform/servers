cbb858a5f5185d5504ebecbb4a573c580d6270f0
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-01 18:40:27 -0400

    #club/tier - updated Tier struct

--- swyl-club-ms/models/swyl.models .go
+++ swyl-club-ms/models/swyl.models .go
@@ -8,6 +8,8 @@
 // @package
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // @notice The information related to a Swyl club
 type Club struct {
 	Club_owner					*string		`json:"club_owner" bson:"club_owner" validate:"required,len=42,alphanum"`
@@ -18,15 +20,15 @@ type Club struct {
 
 // @notice The information related to a Swyl Tier
 type Tier struct {
-	Club_owner					*string 	`json:"club_owner" bson:"club_owner" validate:"required,len=42,alphanum"`
-	// Tier_ID						*uint64		`json:"tier_id" bson:"tier_id" validate:"required"`
-	Tier_name					*string		`json:"tier_name" bson:"tier_name" validate:"required,min=2,max=20"`
-	Tier_img					*string		`json:"tier_img" bson:"tier_img"`
-	Tier_bio					*string		`json:"tier_bio" bson:"tier_bio" validate:"omitempty,min=2,max=200"`
-	Tier_fee					*uint64		`json:"tier_fee" bson:"tier_fee" validate:"required"`
-	Tier_limit					*uint64		`json:"tier_limit" bson:"tier_limit" validate:"required,ne=0"`
-	Tier_welcome_msg				*string		`json:"tier_welcome_message" bson:"tier_welcome_message" validate:"omitempty,min=2,max=100"`
-	Created_at					uint64		`json:"created_at" bson:"created_at"`
+	Club_owner					*string 			`json:"club_owner" bson:"club_owner" validate:"required,len=42,alphanum"`
+	Tier_ID						primitive.ObjectID		`json:"tier_id" bson:"_id, omitempty"`
+	Tier_name					*string				`json:"tier_name" bson:"tier_name" validate:"required,min=2,max=20"`
+	Tier_img					*string				`json:"tier_img" bson:"tier_img"`
+	Tier_bio					*string				`json:"tier_bio" bson:"tier_bio" validate:"omitempty,min=2,max=200"`
+	Tier_fee					*uint64				`json:"tier_fee" bson:"tier_fee" validate:"required"`
+	Tier_limit					*uint64				`json:"tier_limit" bson:"tier_limit" validate:"required,ne=0"`
+	Tier_welcome_msg				*string				`json:"tier_welcome_message" bson:"tier_welcome_message" validate:"omitempty,min=2,max=100"`
+	Created_at					uint64				`json:"created_at" bson:"created_at"`
 }
 
 
