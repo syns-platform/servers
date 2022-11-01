8d3915109acc7134f0cd8ea5b12e6f5b363a9042
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-01 00:14:32 -0400

    #club/tier - infrastructure set up

---
+++ swyl-club-ms/controllers/tier/swyl.tier-controller.go
@@ -0,0 +1,9 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package controllers
---
+++ swyl-club-ms/dao/tier/swyl.tier-dao-impl.go
@@ -0,0 +1,10 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package dao
+
