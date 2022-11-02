54ccae7afddbfb4d3e185830d33634962c9f7924
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-02 12:41:57 -0400

    #comm/comm - init comm-model

---
+++ swyl-community-ms/dao/community/swyl.comm-dao.go
@@ -0,0 +1,51 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package dao
+
+import "Swyl/servers/swyl-community-ms/models"
+
+// @notice Dao interface
+type CommDao interface {
+
+	// @notice Lets a Swyl user create a community
+	// 
+	// @NOTE Should be fired off when #user/connect api is called
+	// 
+	// @param commOwner *string
+	// 
+	// @return error
+	CreateComm(commOwner *string) error
+
+	
+	// @notice Gets a Comm owned by commOwner
+	// 
+	// @param commOwner *string
+	// 
+	// @return *models.Community
+	// 
+	// @return error
+	GetCommOwnedBy(commOwner *string) (*models.Community, error)
+
+
+	// @notice Gets all Comm has ever created
+	// 
+	// @NOTE Might not be necessary
+	// 
+	// return *[]models.Community
+	GetAllComms() (*models.Community, error)
+
+
+	// @notice Updates Comm's total_followers || Comm's total_posts
+	// 
+	// @param commOwner *string
+	// 
+	// @return error
+	UpdateComm(commOwner *string) error
+	
+}
