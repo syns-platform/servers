4807a36b4388e8c90ba2342d59d541beb34220b8
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-02 10:38:56 -0400

    #comm - implemented gin & goenv logic to main

--- swyl-community-ms/swyl.community-main.go
+++ swyl-community-ms/swyl.community-main.go
@@ -8,9 +8,50 @@
 // @package
 package main
 
-import "log"
+// @improt
+import (
+	"Swyl/servers/swyl-community-ms/utils"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @notice global variables
+var (
+	server			*gin.Engine
+)
+
+// @dev Runs before main()
+func init() {
+	// load env variables
+	if (os.Getenv("GIN_MODE") != "release") {utils.LoadEnvVars()}
+
+	// set up gin engine
+	server = gin.Default()
+
+	// Gin trust all proxies by default and it's not safe. Set trusted proxy to home router to to mitigate 
+	server.SetTrustedProxies([]string{os.Getenv("HOME_ROUTER")})
+}
+
 
 // @dev Root function
 func main() {
-	log.Println("swyl-v1.0")
+	// catch all unallowed HTTP methods sent to the server
+	server.HandleMethodNotAllowed = true
+
+	// init basePath
+	basePath := server.Group("v1/swyl/community")
+
+	// basic route
+	basePath.GET("/whort", func(gc *gin.Context) {
+		gc.JSON(200, gin.H{"msg": "hort?"})
+	})
+
+	// run server 
+	if (os.Getenv("GIN_MODE") != "release") {
+		server.Run(os.Getenv("SOURCE_IP"))
+	} else {
+		server.Run(":"+os.Getenv("PRODUCTION_PORT"))
+	}
+	
 }
