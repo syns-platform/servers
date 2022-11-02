fdc46c731055d3aa79b14347a71c89ef59edce36
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-02 13:11:00 -0400

    #comm/comm - init comm-router

---
+++ swyl-community-ms/routers/community/swyl.comm-router.go
@@ -0,0 +1,38 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package routers
+
+// @import
+import (
+	controllers "Swyl/servers/swyl-community-ms/controllers/community"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @notice Root struct for other methods in comm- router
+type CommRouter struct {
+	CommController *controllers.CommController
+}
+
+// @dev Constructor
+func CommRouterConstructor(commController *controllers.CommController) *CommRouter{
+	return &CommRouter{
+		CommController: commController,
+	}
+}
+
+// @notice Method of CommRouter struct
+// 
+// @dev Declares a list of routes
+func (cr *CommRouter) CommRoutes(rg *gin.RouterGroup) {
+	rg.POST("create-community", cr.CommController.CreateComm)
+	rg.GET("get-comminity-owned-by", cr.CommController.GetCommOwnedBy)
+	rg.GET("get-all-comms", cr.CommController.GetAllComms)
+	rg.PATCH("update-comm-owned-by", cr.CommController.UpdateCommOwnedBy)
+}
