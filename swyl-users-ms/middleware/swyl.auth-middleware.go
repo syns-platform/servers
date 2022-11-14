d40db94c7773ac161990123b572a37fd5b0eafbc
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-14 00:41:09 -0500

    Implemented jwt parsing logic

--- swyl-users-ms/middleware/swyl.auth-middleware.go
+++ swyl-users-ms/middleware/swyl.auth-middleware.go
@@ -8,13 +8,44 @@
 // @package
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
 
 // @dev Examine access token in headers
 //
 // @return gin.HandlerFunc
 func Authenticate() gin.HandlerFunc {
 	return func (gc *gin.Context) {
-		// @TODO examine jwt token
+
+            // Get bearer token from Authorization headers
+            bearerToken := gc.GetHeader("Authorization")
+
+            accessToken := strings.Split(bearerToken, " ")[1]
+
+            // Decode/validate accessToken
+            token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+               if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+                  gc.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "!ACCESS_TOKEN - cannot parse token"}); return nil, nil;
+               }
+
+               return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+            })
+            
+            // Implement authenticating logic
+            if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+               log.Println(claims["signature"])
+               log.Println(claims["loginMessage"])
+               log.Println(claims["signer"])
+            } else {
+               gc.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()}); return;
+            }
+		
 	}
 }
