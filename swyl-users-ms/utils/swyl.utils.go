8a3c692f76af303d8ed8a491ad4805ae9ea663bd
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-13 20:10:12 -0500

    Added SetCorsConfig to util

--- swyl-users-ms/utils/swyl.utils.go
+++ swyl-users-ms/utils/swyl.utils.go
@@ -13,7 +13,10 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"time"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -50,4 +53,17 @@ func TestEthAddress(wallet_address *string) (bool, error) {
 func TestSignature(signature *string) (bool, error) {
 	pattern := os.Getenv("SIGNATURE_REGEX")
 	return regexp.MatchString(pattern, *signature)
+}
+
+// @dev Sets up config for cors
+// 
+// @return gin.HandlerFunc
+func SetupCorsConfig() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins: 		[]string{os.Getenv("CORS_ALLOW_ORIGIN")},
+		AllowMethods:		[]string{"POST", "PATCH", "PUT", "DELETE", "GET"},
+		AllowHeaders: 		[]string{"Origin"},	
+		AllowCredentials: 	true,
+		MaxAge: 			12*time.Hour,
+	})
 }
