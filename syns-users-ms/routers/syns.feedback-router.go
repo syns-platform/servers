8b26a8e17282f97146e46197ff69d4998edaefba
Logan Nguyen <lg.131.dev@gmail.com>
2023-04-14 16:15:47 -0400

    feat: added new feedback routes

---
+++ syns-users-ms/routers/syns.feedback-router.go
@@ -0,0 +1,36 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package routers
+
+// @imports
+import (
+	"Syns/servers/syns-users-ms/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @notice Root struct for other methods in router
+type FeedbackRouter struct {
+   FeedbackController *controllers.FeedbackController
+}
+
+// @dev Constructor
+func FeedbackRouterConstructor(feedbackController *controllers.FeedbackController) *FeedbackRouter {
+   return &FeedbackRouter{
+      FeedbackController: feedbackController,
+   }
+}
+
+
+// @notice Method of UserController struct
+// 
+// @dev Declares a list of endpoints
+func (fr * FeedbackRouter) FeedbackRoutes(rg *gin.RouterGroup) {
+   rg.POST("/submit-feedback", fr.FeedbackController.SubmitFeedback)
+}
