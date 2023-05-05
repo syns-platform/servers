961cce9434433e6a29868b386df60a62939e7f03
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-05 16:23:45 -0400

    feat: added new AuthenticateSynsAPIKey() middleware

--- syns-tokens-ms/middleware/syns.auth-middleware.go
+++ syns-tokens-ms/middleware/syns.auth-middleware.go
@@ -9,83 +9,34 @@
 package middleware
 
 import (
-	"net/http"
 	"os"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
 )
 
-// @dev Examine access token in headers
+// @dev Examne Syns API Key in headers
 //
-// @return gin.HandlerFunc
-func Authenticate() gin.HandlerFunc {
-	return func (gc *gin.Context) {
-            // prepare custome claim for decoding
-            type SynsClaims struct {
-                  UserWalletAddress       string `json:"userWalletAddress"`
-                  Signature               string `json:"signature"`
-                  LoginMessage            string `json:"loginMessage"`
-                  jwt.StandardClaims
-            }
-
+// @return gin.HandleFunc
+func AuthenticateSynsAPIKey() gin.HandlerFunc {
+      return func (gc *gin.Context) {
             // Get bearer token from Authorization headers
             bearerToken := gc.GetHeader("Authorization")
             if bearerToken == "" {gc.AbortWithStatusJSON(401, gin.H{"error": "!BEARER_TOKEN - no authorization header found"});return;}
 
-            // extra the jwt from bearer token
-            accessToken := strings.Split(bearerToken, " ")[1]
+            // extract the API_KEY from bearer token
+            clientAPIKey := strings.Split(bearerToken, " ")[1]
 
             // check if accessToken is not empty
-            if accessToken == "" {gc.AbortWithStatusJSON(401, gin.H{"error": "!ACCESS_TOKEN - empty authorization token"})}
+            if clientAPIKey == "" {gc.AbortWithStatusJSON(401, gin.H{"error": "!ACCESS_TOKEN - empty authorization token"})}
 
-            // Decode/validate accessToken
-            token, err := jwt.ParseWithClaims(accessToken, &SynsClaims{}, func(token *jwt.Token) (interface{}, error) {
-               return []byte(os.Getenv("JWT_SECRET_KEY")), nil
-            })
-
+            // compare clientAPIKey with the API Key from server
+            // if don't match, abort request with 401
+            if strings.Compare(clientAPIKey, os.Getenv("SYNS_PLATFORM_API_KEY")) != 0 {
+                  gc.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized"})
+            } 
             
-            
-            // Implement authenticating logic
-            if claims, ok := token.Claims.(*SynsClaims); ok && token.Valid {
-                  // prepare signature & loginMessage as bytes
-                  byteSignature := hexutil.MustDecode(claims.Signature) // decode signature from ahex string with 0x prefix to []byte
-                  byteLoginMessage := []byte(claims.LoginMessage)
-
-                  // calculate a hash for byteLoginMessage which can be safely used to calculate a signature from
-                  hashLoginMessage := accounts.TextHash(byteLoginMessage)
-
-                  if byteSignature[crypto.RecoveryIDOffset] == 27 || byteSignature[crypto.RecoveryIDOffset] == 28 {
-                        byteSignature[crypto.RecoveryIDOffset] -= 27 // Transform yellow paper V from 27/28 to 0/1
-                  }
-                  
-                  // verify the signature to find the pubKey created the signature
-                  ecdsaPubKey, err := crypto.SigToPub(hashLoginMessage, byteSignature)
-                  if err != nil {gc.AbortWithStatusJSON(401, gin.H{"error": err.Error()}); return;}
-
-                  // convert edcsa pubKey to common eth pubKey
-                  pubKeyAddress := crypto.PubkeyToAddress(*ecdsaPubKey)
-                  
-                  // compare pubKeyAddress to claims.UserWalletAddress, 
-                  // if matched => authentication passed, 
-                  // if not matched => authentication failed
-                  if matched := strings.EqualFold(claims.UserWalletAddress, pubKeyAddress.Hex()); !matched {
-                        gc.AbortWithStatusJSON(401, gin.H{"error": "!PUBLIC_KEY - jwt.payload.UserWalletAddress do not match the public key address recovered from verifying jwt.payload.Signature & jwt.payload.LoginMessage"}); 
-                        return;
-                  }
-
-                  // pass the UserWalletAddress to the controller handler function
-                  gc.Set("verifiedUserWalletAddress", claims.UserWalletAddress)
-
-                  // move on
-                  gc.Next()
-            } else {
-               gc.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()}); return;
-            }
-		
-	}
+            // move on
+            gc.Next()
+      }
 }
