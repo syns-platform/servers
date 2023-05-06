3ee08c44003cf3265bbf900fba1a39e95142e318
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-06 14:29:00 -0400

    update: update validator for Syns721SuperNFT struct

--- syns-tokens-ms/models/syns.token-model.go
+++ syns-tokens-ms/models/syns.token-model.go
@@ -42,14 +42,14 @@ type Syns721SuperNFT struct {
 	ERCType          string    `json:"ercType" bson:"erc_type" validate:"required"`
 	Quantity         int       `json:"quantity" bson:"quantity" validate:"required,number"`
 	IsListing        bool      `json:"isListing" bson:"is_listing"`
-	ListingID        int       `json:"listingId" bson:"listing_id" validate:"required,number"`
-	RoyaltyBps       int       `json:"royaltyBps" bson:"royalty_bps" validate:"required,number"`
+	ListingID        int       `json:"listingId" bson:"listing_id" validate:"omitempty,number"`
+	RoyaltyBps       int       `json:"royaltyBps" bson:"royalty_bps" validate:"omitempty,number"`
 	Name             string    `json:"name" bson:"name" validate:"required"`
 	Description      string    `json:"description" bson:"description" validate:"omitempty"`
 	Age              int       `json:"age" bson:"age" validate:"required,number"`
 	SharableLink     string    `json:"sharableLink" bson:"sharable_link" validate:"required"`
 	Lister			 string	   `json:"lister" bson:"lister" validate:"required,eth_addr"`
-	StartSale		 int	   `json:"startSale" bson:"start_sale" validate:"required,number"`
+	StartSale		 int	   `json:"startSale" bson:"start_sale" validate:"omitempty,number"`
 	EndSale			 int	   `json:"endSale" bson:"end_sale" validate:"required,number"`
 	Currency		 string	   `json:"currency" bson:"currency" validate:"required,eth_addr"`
 	BuyouPricePerToken	string	   `json:"buyoutPricePerToken" bson:"buyout_price_per_token" validate:"omitempty"`
