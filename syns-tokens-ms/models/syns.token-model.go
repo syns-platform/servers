5b422064cda2d40eecb0259999a8adb2c08f7a08
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-08 18:44:42 -0400

    update: update struct Syns721SuperNFT

--- syns-tokens-ms/models/syns.token-model.go
+++ syns-tokens-ms/models/syns.token-model.go
@@ -38,7 +38,7 @@ type SynsNFT struct {
 // @notice struct for Syns Token
 type Syns721SuperNFT struct {
 	TokenHash		 string    `json:"token_hash" bson:"token_hash" validate:"omitempty"`
-	TokenID          int       `json:"tokenId" bson:"token_id" validate:"omitempty,number"`
+	TokenID          uint64       `json:"tokenId" bson:"token_id" validate:"omitempty,number"`
 	AssetContract    string    `json:"assetContract" bson:"asset_contract" validate:"required,eth_addr"`
 	TokenOwner       string    `json:"tokenOwner" bson:"token_owner" validate:"required,eth_addr"`
 	OriginalOwner    string    `json:"originalOwner" bson:"original_owner" validate:"required,eth_addr"`
@@ -46,17 +46,17 @@ type Syns721SuperNFT struct {
 	Image            string    `json:"image" bson:"image" validate:"required"`
 	Audio            string    `json:"audio" bson:"audio" validate:"required"`
 	ERCType          string    `json:"ercType" bson:"erc_type" validate:"required"`
-	Quantity         int       `json:"quantity" bson:"quantity" validate:"required,number"`
+	Quantity         uint8       `json:"quantity" bson:"quantity" validate:"required,number"`
 	IsListing        bool      `json:"isListing" bson:"is_listing"`
-	ListingID        int       `json:"listingId" bson:"listing_id" validate:"omitempty,number"`
-	RoyaltyBps       int       `json:"royaltyBps" bson:"royalty_bps" validate:"omitempty,number"`
+	ListingID        uint64       `json:"listingId" bson:"listing_id" validate:"omitempty,number"`
+	RoyaltyBps       uint8       `json:"royaltyBps" bson:"royalty_bps" validate:"omitempty,number"`
 	Name             string    `json:"name" bson:"name" validate:"required"`
 	Description      string    `json:"description" bson:"description" validate:"omitempty"`
 	Age              uint64       `json:"age" bson:"age" validate:"required,number"`
 	SharableLink     string    `json:"sharableLink" bson:"sharable_link" validate:"required"`
 	Lister			 string	   `json:"lister" bson:"lister" validate:"required,eth_addr"`
 	StartSale		 uint64	   `json:"startSale" bson:"start_sale" validate:"omitempty,number"`
-	EndSale			 int64	   `json:"endSale" bson:"end_sale" validate:"required,number"`
+	EndSale			 uint64	   `json:"endSale" bson:"end_sale" validate:"omitempty,number"`
 	Currency		 string	   `json:"currency" bson:"currency" validate:"required,eth_addr"`
 	BuyouPricePerToken	string	   `json:"buyoutPricePerToken" bson:"buyout_price_per_token" validate:"omitempty"`
 }
