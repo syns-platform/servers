17c5496f046d38d3c337d651115a09fb393d10da
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-15 17:36:48 -0500

    feat: created new demorequest-controller

---
+++ syns-users-ms/controllers/syns.demorequest-conlertrol.go
@@ -0,0 +1,58 @@
+/*
+   @dev: Logan (Nam) Nguyen
+   @course: SUNY Oswego - CSC 495 - Capstone
+   @instructor: Professor Bastian Tenbergen
+   @version: 1.0
+*/
+
+// @package
+package controllers
+
+import (
+	"Syns/servers/syns-users-ms/dao"
+	"Syns/servers/syns-users-ms/models"
+	"Syns/servers/syns-users-ms/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @notice Root struct for other methods in controller
+type DemoRequestController struct {
+	DemoRequestDao dao.DemoRequestDao
+}
+
+// @dev Constructor
+func DemoRequestControllerConstructor(demoRequestDao dao.DemoRequestDao) *DemoRequestController{
+	return &DemoRequestController {
+		DemoRequestDao: demoRequestDao,
+	}
+}
+
+// @route `POST/submit-demo-request`
+//
+// @dev handle demo request submission from users
+//
+// @param gc *gin.Context
+func (fc *DemoRequestController) SubmitDemoRequest(gc *gin.Context) {
+
+	// declare param
+	var param *models.DemoRequest
+
+	// bind json post data to param
+	if err := gc.ShouldBindJSON(&param); err != nil {
+		gc.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error":  err.Error()}); return;
+	}
+
+	// struct validation
+	if err := validate.Struct(param); err != nil {gc.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return;}
+
+	// invoke DemoRequestDao.SubmitDemoRequest() api
+	if err := fc.DemoRequestDao.SubmitDemoRequest(&param.Email, &param.Name, &param.Question); err != nil {gc.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()}); return;}
+
+	// alert new demo request submitted
+	utils.EmailNotification("DEMO_REQUEST", param);
+
+	// http response
+	gc.JSON(200,  gin.H{"msg": "Demo request successfully submitted"})
+}
